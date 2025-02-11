e3735e829a5b61e489295b5b3bac2efe8307b6fe
AbdullahNarsun <65645990+ZakriyaAmir@users.noreply.github.com>
2025-02-11 19:05:36 +0500

    added molecule anim

--- Assets/scripts/moleculeBehavior.cs
+++ Assets/scripts/moleculeBehavior.cs
@@ -7,25 +7,48 @@ public class moleculeBehavior : MonoBehaviour
     public float forceAmount = 5f; // Adjust the force strength
     public float destroyTime = 3f; // Time before the objects get destroyed
     public Transform atomsParent;
+    private Vector3 originalScale;
 
     void Start()
     {
+        originalScale = transform.localScale;
         init();
     }
 
     public void init()
     {
-        ApplyRandomForceToChildren();
+        transform.localScale = originalScale;
+        StartCoroutine(ApplyRandomForceToChildren());
     }
 
-    void ApplyRandomForceToChildren()
+    IEnumerator ApplyRandomForceToChildren()
     {
+        float timedelay = Random.Range(1f, 2f);
+        yield return new WaitForSeconds(timedelay);
+
+        //for scale anim
+        Vector3 startScale = transform.localScale;
+        Vector3 endScale = Vector3.zero;
+        float elapsedTime = 0f;
+        float duration = 1f;
+
+        while (elapsedTime < duration)
+        {
+            transform.localScale = Vector3.Lerp(startScale, endScale, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = endScale;
+        //
+
         Debug.Log("zak1");
         foreach (Transform child in atomsParent) // Loop through all children
         {
-            GameObject go = Instantiate(child.gameObject);
+            GameObject go = Instantiate(child.gameObject, transform);
             Debug.Log("zak2 = " + go.name);
             go.transform.parent = null;
+            go.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
             Rigidbody2D rb = go.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
